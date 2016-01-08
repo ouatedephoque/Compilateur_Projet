7438b07b8963e5c70da5d6be755ee0cf24c1c04c
leonardodistasio <leonardo.distasio@he-arc.ch>
2016-01-08 19:21:53 +0100

    Ajouter if, else, while, for Enlever les lexeme non utilisé

--- ProjetSQL/class.java
+++ ProjetSQL/class.java
@@ -1,11 +1,26 @@
 class Test
 {
+	public int id_test = 1;
 	public String test2, salut;
-	public Integer ciao;
+	public Integer ciao = 5;
 	
 	private void test(int hi, int sasda)
 	{
+	}
 	
+	public String getTest2()
+	{
+		return test2;
+	}
+	
+	public void setTest2(String test2)
+	{
+		test2 = test2;
+	}
+	
+	public void toString()
+	{
+		print(test2);
 	}
 	
 	private int hello;
