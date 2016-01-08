0aedd896ff37b208e5dd50d487fb8701093c8911
Jeshon Assunçao <jeshon.assuncao@he-arc.ch>
2016-01-08 08:20:20 +0100

    boucle for and while

--- ProjetSQL/parser.py
+++ ProjetSQL/parser.py
@@ -64,12 +64,58 @@ def p_function_declaration(p):
     """statement : IDENTIFIER expression bloc"""
 
 def p_while_declaration(p):
-    """statement : WHILE expression bloc"""
+    """statement : WHILE condition_while bloc"""
+
+def p_for_declaration(p):
+    """statement : FOR condition_for bloc"""
+
+def p_while_condition(p):
+    """condition_while : '(' while_control ')'"""
+
+def p_for_condition(p):
+    """condition_for : '(' for_control ')' statement
+                        | '(' for_control ')'"""
+
+def p_for_control(p):
+    """for_control : assignation ';' expression_boucle ';' expression_for_inc"""
+
+def p_for_expression_in(p):
+    """expression_for_inc : expression1 operateurInc expression1"""
+
+def p_for_expression1(p):
+    """expression1 : IDENTIFIER
+                    | NUMBER"""
+
+def p_while_control(p):
+    """while_control : expression_boucle"""
+
+def p_boucle_expression(p):
+    """expression_boucle : expression1 operateur expression1"""
+
+def p_operateur(p):
+    """operateur : '='
+                    | '<'
+                    | '>'
+                    | '!'
+                    | '=' operateur
+                    | '<' operateur
+                    | '>' operateur
+                    | '!' operateur """
+
+def p_operateur_for(p):
+    """operateurInc : '+'
+                    | '-'
+                    | '/'
+                    | '*'
+                    | '='
+                    | '+' operateurInc
+                    | '-' operateurInc
+                    | '/' operateurInc
+                    | '*' operateurInc """
 
 def p_expression_className(p):
     """expression : IDENTIFIER"""
     p[0] = AST.TokenNode(p[1])
-
 
 def p_expression_number(p):
     """expression : NUMBER
